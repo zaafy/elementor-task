29f250e34c8877615a5440e703be928a5f7a9d08
Daniel Okoń <daniel.okon@volven.net>
2023-03-30 17:41:34 +0200

    add user wp-test, hide admin bar for this user

--- wp-content/themes/twentytwenty-child/functions.php
+++ wp-content/themes/twentytwenty-child/functions.php
@@ -1,6 +1,7 @@
 <?php
 // Load styles of both parent and child themes
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
 function my_theme_enqueue_styles() {
 	$parenthandle = 'twentytwenty-style';
 	$theme = wp_get_theme();
@@ -13,3 +14,35 @@ function my_theme_enqueue_styles() {
 		array( $parenthandle )
 	);
 }
+
+// 3.1 Create a new user
+add_action('init', 'emt_create_user_with_role');
+
+function emt_create_user_with_role() {
+	define( 'IN_CODE', '1' );
+	include( 'includes/passwd.php' );
+
+    $username = 'wp-test';
+    $email = 'wptest@elementor.com';
+    $password = WP_TEST_PASS;
+
+    $user_id = username_exists( $username );
+    if ( !$user_id && email_exists($email) == false ) {
+        $user_id = wp_create_user( $username, $password, $email );
+        if( !is_wp_error($user_id) ) {
+            $user = get_user_by( 'id', $user_id );
+            $user->set_role( 'editor' );
+        }
+    }
+}
+
+// 3.2 Disable admin bar for wp-test user
+add_action('after_setup_theme', 'emt_remove_admin_bar_for_wptest_user');
+
+function emt_remove_admin_bar_for_wptest_user() {
+	$user = wp_get_current_user();
+	if ($user->user_login == 'wp-test') {
+        show_admin_bar(false);
+	}
+}
+
---
+++ wp-content/themes/twentytwenty-child/includes/passwd.php
@@ -0,0 +1,3 @@
+<?php
+if( ! defined('IN_CODE') ) die();
+define( 'WP_TEST_PASS', '123456789' );
